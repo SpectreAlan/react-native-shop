ec623c48fbe797c029647684bcd9546353882751
Givingcode <giveuptocode@gmail.com>
2020-02-11 21:11:47 +0800

    init project

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <View>
+        <Text>34645645654</Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  topView: {
+    backgroundColor: 'red',
+  },
+});
+
+export default App;
